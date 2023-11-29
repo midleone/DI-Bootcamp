0199361976d19f09c02dcbecd9df22ab76269f8a
DIANA AGEEVA <dianaageeva@Dianas-MacBook-Pro.local>
2023-11-29 11:22:35 +0200

    thritd day

---
+++ week1/day4/home work4.py
@@ -0,0 +1,14 @@
+#Exercise 1
+#
+# def display_message():
+#     print("i understand nothing in phyton, lists, loops :)")
+#
+# display_message()
+
+
+#EX 2
+
+# def favorite_book(title):
+#     print(f"One of my favorite books is {title}")
+#
+# favorite_book("Harry poter")
